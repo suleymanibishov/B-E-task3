a8681aa8a51724835872edae45fa1b49f692c5ed
suleymanibishov <suleyman22022@gmail.com>
2023-11-01 09:05:28 +0400

    Tasks

---
+++ B-E-Task3/B-E-Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace B_E_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (CalcAvg(60, 60, 60) > 60)
+            {
+                Console.WriteLine("Mezun Oldunuz");
+            }
+            else
+            {
+                Console.WriteLine("Tesufki Mezun Ola bilmediniz");
+            }
+            Console.WriteLine(IsPrime(18));
+        }
+        static bool IsPrime(int x)
+        {
+            if (x < 4) return true;
+            for (int i = 4; i < x; i++)
+            {
+                if (x % i == 0) return false;
+            }
+            return true;
+        }
+        static double CalcAvg(params double[] arr) 
+        {
+            double sum = 0;
+            foreach (int x in arr)
+            {
+                sum += x;
+            }
+            return sum / arr.Length;
+        }
+    }
+}
